f9d4c56cfcf62471f2e6d1abd02b495e9a34057f
Tim Monko <timmonko@gmail.com>
2025-12-07 22:06:39 -0600

    drop tests refactored out to other libs

--- tests/test_import.py
+++ tests/test_import.py
@@ -10,3 +10,16 @@ def test_import_time():
     import_time = end_time - start_time
 
     assert import_time < 1.0, 'napari_ndev took too long to import'
+
+def test_widget_time(make_napari_viewer):
+
+    start_time = time.time()
+
+    viewer = make_napari_viewer()
+    viewer.window.add_plugin_dock_widget('napari-ndev', 'nDev App')
+
+    end_time = time.time()
+
+    creation_time = end_time - start_time
+
+    assert creation_time < 3.0, 'nDev App took too long to create'
